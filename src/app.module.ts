d44a18138746a482009be3529f7928a8546caf3e
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-12-22 11:25:32 -0800

    remove unused dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
     imports: [
       MongooseModule.forRoot('mongodb://localhost:27017/nest'),
-      ChatModule,
+      ChatModule
   ],
 })
 export class AppModule {}
