1321c104d78a03770b29f773544153a2918f1982
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-12-21 18:47:19 -0800

    add mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ChatModule } from './chat/chat.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ChatModule],
+    imports: [
+      MongooseModule.forRoot('mongodb://localhost:27017/nest'),
+      ChatModule,
+  ],
 })
 export class AppModule {}
