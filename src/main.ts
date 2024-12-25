06b1f79e19635519167e71731f3bb7238e3b06c6
FrankSkep <f.cornejo.pacheco@gmail.com>
2024-12-24 20:34:04 -0800

    enabling cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
-}
+
+    // Enable CORS for all routes
+    app.enableCors({
+        origin: 'http://localhost:5173',  // Aquí va la URL de tu frontend
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+        credentials: true,  // Si usas cookies o autenticación de sesión
+    });
+
+    // Enable CORS for WebSockets (Socket.io)
+    app.useWebSocketAdapter(new IoAdapter(app));
+    
+    await app.listen(3000);  // Puerto de tu aplicación
+    }
 bootstrap();
